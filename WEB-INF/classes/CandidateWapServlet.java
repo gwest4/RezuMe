bd212fbc40258ea25a31e8385ff02e73b879e5b6
AdityaNarravula <aditya.narravula@gmail.com>
2015-05-18 19:33:19 -0500

    WAP Servlet
    Work in progress it compiles file.

--- WEB-INF/classes/CandidateWapServlet.java
+++ WEB-INF/classes/CandidateWapServlet.java
@@ -1,12 +1,14 @@
-import database.*;
 
-import provider.*;
 
 import java.io.*;
+import java.util.Scanner;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 
+import database.*;
+
+import provider.*;
 /**
  * CandidateWapServlet.
  * 
@@ -24,8 +26,21 @@ public class CandidateWapServlet extends HttpServlet {
     System.out.println(request.getParameterValues("selectedskillsinput"));
     
     StringBuilder htmlStringBuilder = new StringBuilder(HtmlProvider.getInstance().getHtmlHead("candidate-wap.css"));
-
-    
+	
+    htmlStringBuilder.append("<div id=\"body-WAPQuiz\">");
+	htmlStringBuilder.append("<p class=\"body-WAPQuiz-header\">New Candidate WAP Quiz<br><br></em></p>");
+	htmlStringBuilder.append("<form action=\"CandidateWAP2Servlet\" method=\"post\">");
+	htmlStringBuilder.append("<div id=\"WAP-questions\">");
+	
+	//CheckBox Questions
+	
+	Scanner sc = new Scanner(new File("Technology.txt"));
+      while (sc.hasNextLine()) {
+          htmlStringBuilder.append("<p>"+sc.nextLine()+"</p>");
+      }
+	
+	htmlStringBuilder.append("</div></form></div></div>");
+	
     htmlStringBuilder.append(HtmlProvider.getInstance().getHtmlTail());
     out.println(htmlStringBuilder.toString());
     out.close();
