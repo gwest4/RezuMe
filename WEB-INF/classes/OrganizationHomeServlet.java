c014b2d11667e325aca11f814068775d93bf1a55
Adrian Baran <adrian.baran.30@gmail.com>
2015-06-01 19:51:38 -0500

    Set up Organization home to display match results

--- WEB-INF/classes/OrganizationHomeServlet.java
+++ WEB-INF/classes/OrganizationHomeServlet.java
@@ -2,6 +2,8 @@ import database.*;
 import provider.*;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
@@ -24,9 +26,37 @@ public class OrganizationHomeServlet extends HttpServlet {
 
     htmlStringBuilder.append("<div id=\"body-text\">");
     htmlStringBuilder.append(HtmlProvider.getInstance().getOrganizationNavBarHead());
-    
-    htmlStringBuilder.append("<p class=\"body-text-text\">Signed in as <strong>" + DatabaseController.getInstance().getOrganizationName((String) session.getAttribute("currentUser")) + "</strong></p>");
-    htmlStringBuilder.append("</div></div>");
+    htmlStringBuilder.append("<p class=\"body-text-text\">Signed in as <strong>" + 
+        DatabaseController.getInstance().getOrganizationName((String) session.getAttribute("currentUser")) +
+        "</strong><br><br>Here are your matches:</p>");
+    ArrayList<HashMap<String,String>> matches = MatchProvider.getMatches((String) session.getAttribute("currentUser"));
+    htmlStringBuilder.append("\r\n<div id=\"match-box\">\r\n\t"+/*"Sorry, no matches at this time."+*/"\r\n");
+    for (HashMap<String,String> match : matches) {
+      htmlStringBuilder.append("<div id=\"match\">");
+      htmlStringBuilder.append("<table id=\"match-table\">"
+          + "<tr class=\"table-header\">"
+          + "<th>Candidate</td>"
+          + "<th>School</td>"
+          + "<th class=\"long\"></td>"
+          + "<th>Location</td>"
+          + "<th>Phone</td>"
+          + "</tr><tr>"
+          + "<td>"+match.get("")+"</td>" // TODO
+          + "<td>"+match.get("title")+"</td>" // TODO
+          + "<td class=\"long\">"+match.get("")+"</td>" // TODO
+          + "<td>"+match.get("")+", "+match.get("")+"</td>" // TODO
+          + "<td class=\"long\">"+match.get("")+"</td>" // TODO
+          + "</tr></table>");
+      htmlStringBuilder.append("<form class=\"contact-form\" action=\"ContactServlet\" method=\"post\">"
+          + "<input type=\"hidden\" name=\"job-listing-id\" value=\""+match.get(""/* TODO */)+"\"></input>" 
+          + "<input class=\"contact-button\" type=\"submit\""
+            + "value=\"Contact\"></input></form>");
+      htmlStringBuilder.append("<p id=\"match-percentage-text\">"+match.get("")+"% match</p>");
+      htmlStringBuilder.append("</div>");
+    }
+    htmlStringBuilder.append("</div>");
+    htmlStringBuilder.append("</div>");
+    htmlStringBuilder.append("</div>");
 
     htmlStringBuilder.append(HtmlProvider.getInstance().getHtmlTail());
     out.println(htmlStringBuilder.toString());
